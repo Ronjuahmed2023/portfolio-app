34e17525148070e467fe69c10ce3b635c3b8bb68
Ronjuahmed2023 <mdrobiulalomronju@gmail.com>
2023-04-29 01:48:42 +0600

    title changed

--- src/components/WhoAmIText.tsx
+++ src/components/WhoAmIText.tsx
@@ -5,7 +5,7 @@ export const WhoAmIText = () => {
     <div className="text-white">
       <h1 className="text-5xl font-black mb-8 mt-8 text-center sm:text-center md:text-6xl md:mt-0 md:text-left lg:text-7xl lg:mt-0 lg:text-left">
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-indigo-600">Hi there, <br /></span>
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">I'm Polash Ahmad</span>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">I'm Ronju Ahmed</span>
       </h1>
       <p className="text-base mx-4 leading-5 text-center mb-8 sm:text-center sm:mx-0 md:w-11/12 md:text-lg md:text-left lg:w-11/12 lg:text-left">
         I am a full-stack developer and creative designer, I have extensive experience developing projects from ideation to deployment.
