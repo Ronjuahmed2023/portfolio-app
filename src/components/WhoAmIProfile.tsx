5ba5310ff239b7539eca2d0d236b43123a67c653
Ronjuahmed2023 <mdrobiulalomronju@gmail.com>
2023-05-13 02:52:40 +0600

    update the who am i section

--- src/components/WhoAmIProfile.tsx
+++ src/components/WhoAmIProfile.tsx
@@ -4,7 +4,7 @@ export const WhoAmIProfile = () => {
   return (
     <div className="text-white mx-4">
       <div className="">
-        <img className="w-2/3 m-auto rounded-2xl" src="images/polash_ahmad.jpeg" alt="Polash Ahmad" />
+        <img className="w-2/3 m-auto rounded-2xl" src="images/robiul_alom_ronju.jpg" alt="Robiul Alom Ronju" />
       </div>
     </div>
   )
--- src/components/WhoAmIText.tsx
+++ src/components/WhoAmIText.tsx
@@ -3,14 +3,15 @@ export const WhoAmIText = () => {
 
   return (
     <div className="text-white">
-      <h1 className="text-5xl font-black mb-8 mt-8 text-center sm:text-center md:text-6xl md:mt-0 md:text-left lg:text-7xl lg:mt-0 lg:text-left">
+      <h1 className="text-5xl font-black mb-8 mt-8 text-center sm:text-center md:text-6xl md:mt-0 md:text-left lg:text-5xl lg:mt-0 lg:text-left">
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-indigo-600">Hi there, <br /></span>
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">I'm Ronju Ahmed</span>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">I'm Robiul Alom Ronju</span>
       </h1>
-      <p className="text-base mx-4 leading-5 text-center mb-8 sm:text-center sm:mx-0 md:w-11/12 md:text-lg md:text-left lg:w-11/12 lg:text-left">
-        I am a full-stack developer and creative designer, I have extensive experience developing projects from ideation to deployment.
-        I thrive on creative problem-solving, using my technical skills and artistic vision to build elegant solutions.
-        I am always eager to explore new techniques and technologies to further hone my skills.
+      <p className="text-base mx-4 leading-5 text-center mb-8 sm:text-center sm:mx-0 md:w-11/12 md:text-md md:text-left lg:w-11/12 lg:text-left">
+        I’m a  skilled SEO expert with over 3 years of experience who specializes in improving 
+        online visibility through keyword research, on-page optimization, link building, and 
+        technical SEO. I takes a personalized approach to each project, tailoring my strategies 
+        to fit the unique needs of my clients.
       </p>
     </div>
   )
