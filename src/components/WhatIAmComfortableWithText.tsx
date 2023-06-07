917bb4209fbbe84c79c1fd7d6a4e2bbcaf1daa59
Ronjuahmed2023 <mdrobiulalomronju@gmail.com>
2023-06-07 17:36:04 +0600

    update on tools & what do i do section

--- src/components/WhatIAmComfortableWithText.tsx
+++ src/components/WhatIAmComfortableWithText.tsx
@@ -4,8 +4,8 @@ export const WhatIAmComfortableWithText = () => {
   return (
     <div className="text-white mx-4">
       <h1 className="text-5xl font-black mb-8 mt-8 text-center sm:text-center md:text-6xl md:mt-0 md:text-left lg:text-7xl lg:mt-0 lg:text-left">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-indigo-600">What I'm </span>
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">Comfortable With</span>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-indigo-600">What Do </span>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">I Do</span>
       </h1>
     </div>
   )
--- src/components/WhoAmIText.tsx
+++ src/components/WhoAmIText.tsx
@@ -8,7 +8,7 @@ export const WhoAmIText = () => {
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-indigo-800">I'm Robiul Alom Ronju</span>
       </h1>
       <p className="text-base mx-4 leading-5 text-center mb-8 sm:text-center sm:mx-0 md:w-11/12 md:text-md md:text-left lg:w-11/12 lg:text-left">
-        I’m a  skilled SEO expert with over 3 years of experience who specializes in improving 
+        I’m a  skilled SEO expert with over 3 years of experience. Who specializes in improving 
         online visibility through keyword research, on-page optimization, link building, and 
         technical SEO. I takes a personalized approach to each project, tailoring my strategies 
         to fit the unique needs of my clients.
